6c9f2008aaa1f9bb6e9393a6ea0731bd418468ab
callor <callor@callor.com>
2022-03-22 11:50:40 +0900

    java

---
+++ Java_00_Hello/src/com/callor/hello/GuGuDan.java
@@ -0,0 +1,21 @@
+package com.callor.hello;
+
+public class GuGuDan {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("구구단 3단");
+		System.out.println("============================");
+		System.out.println("    3 x 2 = " + 3*2);
+		System.out.println("    3 x 3 = " + 3*3);
+		System.out.println("    3 x 4 = " + 3*4);
+		System.out.println("    3 x 5 = " + 3*5);
+		System.out.println("    3 x 6 = " + 3*6);
+		System.out.println("    3 x 7 = " + 3*7);
+		System.out.println("    3 x 8 = " + 3*9);
+		System.out.println("============================");
+		
+		
+	}
+
+}
--- Java_00_Hello/src/com/callor/hello/HelloKorea.java
+++ Java_00_Hello/src/com/callor/hello/HelloKorea.java
@@ -23,6 +23,16 @@ public class HelloKorea {
 		System.out.println(40 - 30);
 		System.out.println(40 / 2);
 		System.out.println(30 + 40 * 40 / 2 - 100);
+		
+		System.out.println("30 + 40 = " + (30+40));
+		System.out.println("30 + 40 = " + "70");
+		
+		// 문자열 덧셈 연산
+		// 문자열 + 문자열은 문자열을 연결하여 결과를 만든다
+		System.out.println("대한" + "민국");
+		System.out.println("대한민국");
+		
+		
 	}
 }
 
