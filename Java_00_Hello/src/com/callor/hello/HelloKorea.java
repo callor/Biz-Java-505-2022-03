5a522aaa86726222fea72dc304dd9dbbfa0c1c57
callor <callor@callor.com>
2022-03-22 10:37:48 +0900

    java

---
+++ Java_00_Hello/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,23 @@
+/**
+ * 현재 생성한 HelloKorea 클래스는 
+ * com/callor/hello 폴더에 저장되어있다 라는 표식
+ * 현재 생성한 클래스가 저장된 base package 다 라고 한다.
+ */
+package com.callor.hello;
+
+// 지금작성하는 소스코드는 HelloKorea.java 파일이다
+// HelloKorea.java 를 클래스파일 또는 자바소스코드 라고 한다.
+public class HelloKorea {	
+	/**
+	 * main 키워드 + Ctrl + Space : 상용구 작성
+	 * @param args
+	 * 
+	 * Key word, reserved word, 명령어(Command word)
+	 * package, public, class, static, void
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
