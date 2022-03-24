de3345e62a5aab82b7aa98954880593d3f36e907
callor <callor@callor.com>
2022-03-24 11:52:07 +0900

    변수

--- Java_10_Varriable_02/src/com/callor/var/Var_05.java
+++ Java_10_Varriable_02/src/com/callor/var/Var_05.java
@@ -12,9 +12,31 @@ public class Var_05 {
 		intNum1 = 55;
 		intNum2 = 77;
 		intSum = intNum1 + intNum2;
+		System.out.println(intNum1);
+		System.out.println(intNum2);
 		System.out.println(intSum);
 		
+		System.out.print(intNum1);
+		System.out.print(" + ");
+		System.out.print(intNum2);
+		System.out.print(" = ");
+		System.out.println(intSum);
 		
+		System.out.println(intNum1 
+				+ " + " 
+				+ intNum2 
+				+ " = " 
+				+ intSum);
+		intNum1 = 101;
+		intNum2 = 202;
+		System.out.printf("%d + %d = %d\n",
+					intNum1, intNum2, intSum);
+		System.out.printf("%d x %d = %d\n",
+				intNum1, intNum2, intNum1 * intNum2);
+		System.out.printf("%d - %d = %d\n",
+				intNum1, intNum2, intNum1 - intNum2);
+		System.out.printf("%d / %d = %d\n",
+				intNum1, intNum2, intNum1 / intNum2);
 		
 	}
 
---
+++ Java_10_Varriable_02/src/com/callor/var/Var_06.java
@@ -0,0 +1,29 @@
+package com.callor.var;
+
+public class Var_06 {
+	
+	public static void main(String[] args) {
+		
+		int intNum1 = 0;
+		int intNum2 = 0;
+		
+		int intSum = 0; // 합
+		int intDeffrence = 0; // 차(뺄셈한 값)
+		int intProduct = 0; // 곱(곱셈한 값)
+		int intDivide = 0; // 나눗셈
+		int intMod = 0; // 나머지
+		
+		intNum1 = 89;
+		intNum2 = 3;
+		intSum = intNum1 + intNum2;
+		intDeffrence = intNum1 - intNum2;
+		intProduct = intNum1 * intNum2;
+		intDivide = intNum1 / intNum2;
+		intMod = intNum1 % intNum2;
+		
+		System.out.println(intDefference);
+		
+		
+	}
+
+}
