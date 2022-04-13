af008ee3ec3d1e5221c48555f20d309fc07a504f
callor <callor@callor.com>
2022-04-13 12:42:11 +0900

    ServiceClass

--- Java_30_Arrays_04/src/com/callor/arrays/service/ScoreServiceV5.java
+++ Java_30_Arrays_04/src/com/callor/arrays/service/ScoreServiceV5.java
@@ -1,5 +1,18 @@
 package com.callor.arrays.service;
 
+/*
+ * makeScore()
+ * printScore()
+ * sumScore()
+ * 등의 method 를 선언하고
+ * 
+ * 임의 성적을 생성하여 intKor 에 저장
+ * 성적일람표 출력
+ * 총점계산
+ * 
+ */
 public class ScoreServiceV5 {
+	
+	private int[] intKor;
 
 }
