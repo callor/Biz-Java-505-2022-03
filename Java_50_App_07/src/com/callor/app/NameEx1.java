0bfc2aaed97e28f18aecf86e3bcbc1aa4e5a77e3
callor <callor@callor.com>
2022-05-02 16:41:32 +0900

    R

---
+++ Java_50_App_07/src/com/callor/app/NameEx1.java
@@ -0,0 +1,21 @@
+package com.callor.app;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+
+ * 
+ * 
+ */
+public class NameEx1 {
+	
+	public static void main(String[] args) {
+		
+		List<String> names = new ArrayList<>();
+		Collections.shuffle(names);
+		System.out.println(names);
+		
+	}
+}
