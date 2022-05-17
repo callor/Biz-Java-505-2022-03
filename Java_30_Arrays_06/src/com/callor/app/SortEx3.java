e80ab674ac098cc58d844578631db3bbc3d6cb1d
callor <callor@callor.com>
2022-05-17 16:19:06 +0900

    R

---
+++ Java_30_Arrays_06/src/com/callor/app/SortEx3.java
@@ -0,0 +1,22 @@
+package com.callor.app;
+
+public class SortEx3 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("가".compareTo("나"));
+		System.out.println("나".compareTo("가"));
+		System.out.println("가".compareTo("가"));
+		
+		System.out.println("A".compareTo("Z"));
+		System.out.println("Z".compareTo("A"));
+		System.out.println("A".compareTo("A"));
+		
+		System.out.println("A".compareTo("z"));
+		System.out.println("z".compareTo("A"));
+		System.out.println("A".compareTo("a"));
+		
+		
+	}
+
+}
